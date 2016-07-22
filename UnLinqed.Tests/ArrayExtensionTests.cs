076dc13cd4c4c5a87b66feeeeabba1a06d375a7b
João Paulo Figueira <joao.figueira@frotcom.com>
2016-07-22 12:23:08 +0100

    First useful code

---
+++ UnLinqed.Tests/ArrayExtensionTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnLinqed;
+
+namespace UnLinqed.Tests
+{
+    [TestClass]
+    public class ArrayExtensionTests
+    {
+        [TestMethod]
+        public void SkipAndTake_Splices_Start_Succeeds()
+        {
+            int[] test      = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] first3    = test.SkipAndTake(0, 3);
+
+            Assert.AreEqual(test[0], first3[0]);
+            Assert.AreEqual(test[1], first3[1]);
+            Assert.AreEqual(test[2], first3[2]);
+        }
+
+        [TestMethod]
+        public void SkipAndTake_Splices_Middle_Succeeds()
+        {
+            int[] test = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] mid3 = test.SkipAndTake(3, 3);
+
+            Assert.AreEqual(test[3], mid3[0]);
+            Assert.AreEqual(test[4], mid3[1]);
+            Assert.AreEqual(test[5], mid3[2]);
+        }
+
+        [TestMethod]
+        public void SkipAndTake_Splices_End_Succeeds()
+        {
+            int[] test = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] mid3 = test.SkipAndTake(7, 3);
+
+            Assert.AreEqual(test[7], mid3[0]);
+            Assert.AreEqual(test[8], mid3[1]);
+            Assert.AreEqual(test[9], mid3[2]);
+        }
+
+        [TestMethod]
+        public void RemoveNulls_Removes_Start_Succeeds()
+        {
+            string[] array = new string[] { null, "1", "2" };
+            string[] test = array.RemoveNulls();
+
+            Assert.AreEqual(array[1], test[0]);
+            Assert.AreEqual(array[2], test[1]);
+            Assert.AreEqual(test.Length, 2);
+        }
+
+        [TestMethod]
+        public void RemoveNulls_Removes_Middle_Succeeds()
+        {
+            string[] array = new string[] { "0", null, "2" };
+            string[] test = array.RemoveNulls();
+
+            Assert.AreEqual(array[0], test[0]);
+            Assert.AreEqual(array[2], test[1]);
+            Assert.AreEqual(test.Length, 2);
+        }
+
+        [TestMethod]
+        public void RemoveNulls_Removes_End_Succeeds()
+        {
+            string[] array = new string[] { "0", "1", null };
+            string[] test = array.RemoveNulls();
+
+            Assert.AreEqual(array[0], test[0]);
+            Assert.AreEqual(array[1], test[1]);
+            Assert.AreEqual(test.Length, 2);
+        }
+    }
+}
---
+++ UnLinqed/ArrayExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnLinqed
+{
+    public static class ArrayExtensions
+    {
+        public static T[] RemoveNulls<T>(this T[] array) where T : class
+        {
+            T[] temp = new T[array.Length];
+            int c = 0;
+
+            for(int i = 0; i < temp.Length; i++)
+            {
+                if(array[i] != null)
+                {
+                    temp[c++] = array[i];
+                }
+            }
+            return temp.Left(c);
+        }
+
+        public static T[] Left<T>(this T[] array, int length) where T : class
+        {
+            T[] left = new T[length];
+
+            Array.Copy(array, left, length);
+            return left;
+        }
+
+        public static T[] Filter<T>(this T[] array, Func<T,bool> predicate) where T : class
+        {
+            T[] temp = new T[array.Length];
+            int c = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (predicate(array[i]))
+                    temp[c++] = array[i];
+            }
+            return temp.Left(c);
+        }
+
+        public static T[] Slice<T>(this T[] array, int start, int end)
+        {
+            if (start > end)
+                throw new ArgumentOutOfRangeException("The start index is larger than the end index. ");
+            T[] temp = new T[end - start];
+            Array.Copy(array, start, temp, 0, end - start);
+            return temp;
+        }
+
+        public static T[] SkipAndTake<T>(this T[] array, int skip, int take)
+        {
+            T[] temp = new T[take];
+            Array.Copy(array, skip, temp, 0, take);
+            return temp;
+        }
+
+    }
+}
